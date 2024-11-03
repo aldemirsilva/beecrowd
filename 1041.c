bd0915e8b9ce02aae0a6dd746884a7f71fb9be31
Aldemir Rodrigues <aldemir.rodrigues@gmail.com>
2024-11-03 13:13:00 -0400

    Coordenadas de um Ponto

--- 1041.c
+++ 1041.c
@@ -1,13 +1,30 @@
+// author: Aldemir Silva  | date: November 3rd 2024
+
 #include <stdio.h>
 
-int main()
-{
-    double x, y;
-    scanf("%lf %lf", &x, &y);
+int main() {
+  double x, y;
 
-    if (x == 0 && y == 0) printf("Origem\n");
-    else if (x >= 0 && y >= 0) printf("Q1\n");
-    else if (x <= 0 && y >= 0) printf("Q2\n");
-    else if (x <= 0 && y <= 0) printf("Q3\n");
-    else if (x >= 0 && y <= 0) printf("Q4\n");
+  scanf("%lf %lf", &x, &y);
+  if (x == 0.0f && y == 0.0f)
+    printf("Origem\n");
+
+  else if (x > 0.0f && y > 0.0f)
+    printf("Q1\n");
+
+  else if (x < 0.0f && y > 0.0f)
+    printf("Q2\n");
+
+  else if (x < 0.0 && y < 0.0)
+    printf("Q3\n");
+
+  else if (x > 0.0 && y < 0.0)
+    printf("Q4\n");
+
+  else if (y == 0.0f)
+    printf("Eixo X\n");
+
+  else if (x == 0.0f)
+    printf("Eixo Y\n");
+  return 0;
 }
