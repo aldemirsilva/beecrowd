f9a25ee94fdd4909a464818468923c833e9d1821
Aldemir Rodrigues <aldemir.rodrigues@gmail.com>
2024-11-03 15:01:50 -0400

    Tempo de Jogo com Minutos

--- 1047.c
+++ 1047.c
@@ -1,15 +1,38 @@
+// author: Aldemir Silva | date: November 3rd 2024
+
 #include <stdio.h>
 
-int main(void) {
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    int horas = c-a;
-    if (!horas) horas = 24;
-    int minutos = d-b;
-    if (minutos < 0) {
-        horas += minutos;
-        minutos += 60;
-    }
-    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", horas, minutos);
-    return 0;
+int main() {
+  int hora_inicial, minuto_inicial, hora_final, minuto_final;
+
+  scanf("%d %d %d %d", &hora_inicial, &minuto_inicial, &hora_final, &minuto_final);
+
+  int horas = hora_final - hora_inicial;
+  int minutos = minuto_final - minuto_inicial;
+
+  if (horas == 0 && minutos == 0)
+    horas = 24;
+
+  if (horas < 0 && minutos > 0)
+    horas += 24;
+
+  if (horas > 0 && minutos < 0) {
+    horas--;
+    minutos += 60;
+  }
+
+  if (horas < 0 && minutos < 0) {
+    horas += 23;
+    minutos += 60;
+  }
+
+  if (horas == 0 && minutos < 0) {
+    horas = 23;
+    minutos += 60;
+  }
+
+
+  printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", horas, minutos);
+
+  return 0;
 }
