f7ff164e61a1329e85d287e22b3843c68657fc69
Aldemir Rodrigues <aldemir.rodrigues@gmail.com>
2024-11-03 13:49:35 -0400

    Múltiplos

--- 1044.c
+++ 1044.c
@@ -1,9 +1,11 @@
+// author: Aldemir Silva | date: November 3rd 2024
+
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    if (a % b == 0 || b % a == 0) printf("Sao Multiplos\n");
-    else printf("Nao sao Multiplos\n");
-    return 0;
+  int a, b;
+  scanf("%d %d", &a, &b);
+  if (a % b == 0 || b % a == 0) printf("Sao Multiplos\n");
+  else printf("Nao sao Multiplos\n");
+  return 0;
 }
