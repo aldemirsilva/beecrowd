40b9a8eb4b1997c2e8b4eabd3297ef90349f2442
Aldemir Rodrigues <aldemir.rodrigues@gmail.com>
2024-11-03 13:24:36 -0400

    Intervalo

--- 1037.c
+++ 1037.c
@@ -1,18 +1,19 @@
+// author: Aldemir Silva | date: November 3rd 2024
+
 #include <stdio.h>
 
-int main()
-{
-    double entrada;
-    scanf("%lf", &entrada);
+int main() {
+  double entrada;
+  scanf("%lf", &entrada);
 
-    if (entrada >=0 && entrada <= 25)
-        printf("Intervalo [0,25]\n");
-    else if (entrada > 25 && entrada <= 50)
-        printf("Intervalo (25,50]\n");
-    else if (entrada > 50 && entrada <= 75)
-        printf("Intervalo (50,75]\n");
-    else if (entrada > 75 && entrada <= 100)
-        printf("Intervalo (75,100]\n");
-    else
-        printf("Fora de intervalo\n");
+  if (entrada >= 0 && entrada <= 25)
+    printf("Intervalo [0,25]\n");
+  else if (entrada > 25 && entrada <= 50)
+    printf("Intervalo (25,50]\n");
+  else if (entrada > 50 && entrada <= 75)
+    printf("Intervalo (50,75]\n");
+  else if (entrada > 75 && entrada <= 100)
+    printf("Intervalo (75,100]\n");
+  else
+    printf("Fora de intervalo\n");
 }
