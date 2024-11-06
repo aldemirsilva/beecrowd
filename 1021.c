7a0f9ae71d53f127e03ea242653a80940dd2f5b0
Aldemir Rodrigues <aldemir.rodrigues@gmail.com>
2024-11-06 15:39:57 -0400

    Notas e Moedas

--- 1021.c
+++ 1021.c
@@ -1,35 +1,34 @@
 // author: Aldemir Silva | date: November 3rd 2024
 
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main() {
-  char *value = malloc(10 * sizeof(char));
-  unsigned whole, fraction, current;
-  const unsigned notes[] = {100, 50, 20, 10, 5, 2};
-  const unsigned coins[] = {100, 50, 25, 10, 5, 1};
+  char *valor = malloc(10 * sizeof(char));
+  unsigned inteiro, fracao, atual;
+  const unsigned notas[] = {100, 50, 20, 10, 5, 2};
+  const unsigned moedas[] = {100, 50, 25, 10, 5, 1};
 
-  scanf("%s", value);
+  scanf("%s", valor);
 
-  whole = atoi(strtok(value, "."));
-  fraction = atoi(strtok(NULL, "."));
+  inteiro = atoi(strtok(valor, "."));
+  fracao = atoi(strtok(NULL, "."));
 
-  current = whole;
+  atual = inteiro;
 
   printf("NOTAS:\n");
   for (int i = 0; i < 6; i++) {
-    printf("%d nota(s) de R$ %d.00\n", current / notes[i], notes[i]);
-    current %= notes[i];
+    printf("%d nota(s) de R$ %d.00\n", atual / notas[i], notas[i]);
+    atual %= notas[i];
   }
 
-  fraction += current * 100;
+  fracao += atual * 100;
   printf("MOEDAS:\n");
   for (int j = 0; j < 6; j++) {
-    printf("%d moeda(s) de R$ %.2f\n", fraction / coins[j], (float) (coins[j] / 100.00));
-    fraction %= coins[j];
+    printf("%d moeda(s) de R$ %.2f\n", fracao / moedas[j], (float) (moedas[j] / 100.00));
+    fracao %= moedas[j];
   }
-  free(value);
+  free(valor);
   return 0;
 }
